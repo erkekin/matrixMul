22fa98dadd2072ecdcef6b854b737bff3252af3c
erk ekin <erkekin@gmail.com>
2014-03-19 17:22:19 +0200

    Initial Commit

---
+++ OS/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  OS
+//
+//  Created by Erk EKIN on 19/03/14.
+//  Copyright (c) 2014 erkekin. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
