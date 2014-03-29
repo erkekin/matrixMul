6b3e1fc23c21dc8c5ad55f778d208a8eccac00de
erk ekin <erkekin@gmail.com>
2014-03-29 10:09:30 +0200

    read write 3d array

--- OS/main.c
+++ OS/main.c
@@ -56,7 +56,7 @@ void printMatrixes(int N) {
     
     int j;
     int i;
- 
+    
     for (i=0; i<N; i++) {
         
         for (j=0; j<N; j++) {
@@ -109,77 +109,144 @@ void* multiply(void* slice)
 
 #pragma mark Main Function
 
+int ** create_table(char*  filename, int size)
+{
+      FILE * matrixFile;
+    int x;
+    int **array;
+    
+    if ((matrixFile = fopen(filename, "rb")) == NULL)
+    {
+        printf("Error opening file\n");
+        
+    }
+  
+    array = malloc (size*sizeof(int*));
+    
+    for(x = 0; x < size; x++)
+        array[x] = (int*)malloc(sizeof(int)*100);
+    
+        //     fread(array, sizeof(int),  size*size, matrixFile);
+    
+         fclose(matrixFile);
+    
+    return array;
+}
+
+
+
+
 int main(int argc, char* argv[]){
     
-    int i =0;
-    int rowCount =0;
-    struct timeval t0,t1;
+    FILE *matrixFile;
+    int i ,y=0;
+        //    int rowCount =0;
+        //    struct timeval t0,t1;
+        //
+    int **a; int **b;
     
     if (argc < MIN_REQUIRED)  return help();
     
-    size = atoi (argv[1]);
+    size = atoi (argv[1]); num_thrd = atoi(argv[2]);
     
-      int** array = (int**)malloc(size*sizeof(int));
+    a= create_table("matrixa.dat",size);
+    b= create_table("matrixb.dat",size);
     
-    num_thrd = atoi(argv[2]);
-    
-    printf("size: %lu \n", sizeof(**array));
-    
-    if(array == NULL)
+    for(i = 0; i < size; i++)
     {
-		fprintf(stderr, "out of memory\n");
-		
+        for(y = 0; y < size; y++){
+            
+          a[i][y]=i ;
+        }
+        
+        printf("\n");
+        
     }
-	for(i = 0; i < size; i++)
+  
+      //YAZ
+    
+    matrixFile=fopen("matris1.dat", "wb");
+    
+    for(i = 0; i < size; i++)
     {
-		array[i] = malloc(size * sizeof(int));
-		if(array[i] == NULL)
-        {
-			fprintf(stderr, "out of memory\n");
-		
+        
+        fwrite(a,sizeof(int*),size,matrixFile);
+        
+        for (y=0; y<size; y++) {
+            
+             fwrite(a[i],sizeof(int),size,matrixFile);
+        }
+    }
+    
+        //  fwrite(a, sizeof(int*), size*size, matrixFile);
+    
+    fclose(matrixFile);
+
+    
+    
+       //OKU
+    
+    if ((matrixFile = fopen("matris1.dat", "rb")) == NULL)
+    {
+        printf("Error opening file\n");
+      
+    }
+    
+    fread(a, sizeof(int*),  size*size, matrixFile);
+    
+    fclose(matrixFile);
+
+    
+    
+    for(i = 0; i < size; i++)
+    {
+        for(y = 0; y < size; y++){
+            
+            printf("%d\n",a[i][y]=i) ;
+        }
+        
+        printf("\n");
+        
     }
 
     
-    }
     
     
     
     
-    
-    
-//    readMatrix((char*)argv[3],size);
-//    readMatrix((char*)argv[4],size);
-//    
-//    rowCount =size/num_thrd;
-//    
-//    gettimeofday(&t0, 0);
-//
-//    for (i=0; i<num_thrd; i++) {
-//        
-//        pthread_t tid;       //Thread ID
-//        pthread_attr_t attr; //Set of thread attributes
-//        //Get the default attributes
-//        pthread_attr_init(&attr);
-//        //Create the thread
-//        pthread_create(&tid,&attr,multiply,(void*)i);
-//        pthread_join(tid, NULL);
-//        
-//    }
-//    
-//    gettimeofday(&t1, 0);
-//    long elapsed = (t1.tv_sec-t0.tv_sec)*1000000 + t1.tv_usec-t0.tv_usec;
-//    printf("\n\nTotal execution time using threads is %ld microseconds\n",elapsed);
-//
-////        C[0][0] = 1;   C[0][1] = 2;    C[0][2] = 3;   C[0][3] = 4;   C[0][4] = 5;   C[0][5] = 6;
-////        C[1][0] = 7;   C[1][1] = 8;    C[1][2] = 9;   C[1][3] = 10;   C[1][4] = 11;   C[1][5] = 12;
-////        C[2][0] = 1;   C[2][1] = 2;    C[2][2] = 3;   C[2][3] = 4;   C[2][4] = 5;   C[2][5] = 6;
-////        C[3][0] = 1;   C[3][1] = 2;    C[3][2] = 3;   C[3][3] = 4;   C[3][4] = 5;   C[3][5] = 6;
-////        C[4][0] = 1;   C[4][1] = 2;    C[4][2] = 3;   C[4][3] = 4;   C[4][4] = 5;   C[4][5] = 6;
-////        C[5][0] = 1;   C[5][1] = 2;    C[5][2] = 3;   C[5][3] = 4;   C[5][4] = 5;   C[5][5] = 6;
-//
-//   // writeMatrix((char*)argv[5],size);
-//    
-//    printMatrixes(size);
+        //    readMatrix((char*)argv[3],size);
+        //    readMatrix((char*)argv[4],size);
+        //
+        //    rowCount =size/num_thrd;
+        //
+        //    gettimeofday(&t0, 0);
+        //
+        //    for (i=0; i<num_thrd; i++) {
+        //
+        //        pthread_t tid;       //Thread ID
+        //        pthread_attr_t attr; //Set of thread attributes
+        //        //Get the default attributes
+        //        pthread_attr_init(&attr);
+        //        //Create the thread
+        //        pthread_create(&tid,&attr,multiply,(void*)i);
+        //        pthread_join(tid, NULL);
+        //
+        //    }
+        //
+        //    gettimeofday(&t1, 0);
+        //    long elapsed = (t1.tv_sec-t0.tv_sec)*1000000 + t1.tv_usec-t0.tv_usec;
+        //    printf("\n\nTotal execution time using threads is %ld microseconds\n",elapsed);
+        //
+        ////        C[0][0] = 1;   C[0][1] = 2;    C[0][2] = 3;   C[0][3] = 4;   C[0][4] = 5;   C[0][5] = 6;
+        ////        C[1][0] = 7;   C[1][1] = 8;    C[1][2] = 9;   C[1][3] = 10;   C[1][4] = 11;   C[1][5] = 12;
+        ////        C[2][0] = 1;   C[2][1] = 2;    C[2][2] = 3;   C[2][3] = 4;   C[2][4] = 5;   C[2][5] = 6;
+        ////        C[3][0] = 1;   C[3][1] = 2;    C[3][2] = 3;   C[3][3] = 4;   C[3][4] = 5;   C[3][5] = 6;
+        ////        C[4][0] = 1;   C[4][1] = 2;    C[4][2] = 3;   C[4][3] = 4;   C[4][4] = 5;   C[4][5] = 6;
+        ////        C[5][0] = 1;   C[5][1] = 2;    C[5][2] = 3;   C[5][3] = 4;   C[5][4] = 5;   C[5][5] = 6;
+        //
+        //   // writeMatrix((char*)argv[5],size);
+        //
+        //    printMatrixes(size);
     
     return 0;
     
